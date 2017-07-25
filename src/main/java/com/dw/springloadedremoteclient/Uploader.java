0f0f11fcb075c0e12ded26e9b7d5e5d94573f14a
Chirag Moradiya <chirag@dreamworldsolutions.com>
2017-07-25 18:00:43 +0530

    Uploader's javadoc & specification updated

--- src/main/java/com/dw/springloadedremoteclient/Uploader.java
+++ src/main/java/com/dw/springloadedremoteclient/Uploader.java
@@ -1,15 +1,30 @@
 package com.dw.springloadedremoteclient;
 
+import java.io.File;
+
 /**
- * This component used to upload changes on server.
+ * Uploads file to given URL using HTTP PUT. See API doc for more detail.
  * 
  * @author Jaydeep Kumbhani
  *
  */
 public class Uploader implements Listener {
+  private String url;
+  private File baseDir;
+
+
+  /**
+   * 
+   * @param url Server end-point where file is to be uploaded using HTTP PUT.
+   * @param baseDir A directory from which File is read for upload. Note:: {@link Change} event's
+   *        path is starting with '/' though it's not an absolute path. It's a path relative to this
+   *        directory.
+   */
+  public Uploader(String url, File baseDir) {
+    this.url = url;
+  }
 
   public void onChange(Change change) {
-    // TODO upload changes
 
   }
 
